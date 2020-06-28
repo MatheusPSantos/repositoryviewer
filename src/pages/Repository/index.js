ee86f2a4a411f939c6759f1c4e4b22f5a025f694
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2020-06-28 18:16:57 -0300

    carregando dados da api

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
-export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+export default class Repository extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      repository: [],
+      issues: [],
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const { match } = this.props;
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`repos/${repoName}`),
+      api.get(`repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    return <h1>olá</h1>;
+  }
 }
