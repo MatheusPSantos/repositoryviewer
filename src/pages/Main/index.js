86ff131cc82f6ec93311a9a4c9ef81c2e1c271d6
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2020-06-28 13:08:35 -0300

    estilizando a pagina main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Title>Hello World</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => { }}>
+        <input type="text" placeholder="Adicionar repositório" />
+        <SubmitButton disabled>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
