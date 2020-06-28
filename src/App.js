ef05fea2b970b7b7e3ace6fe2b4e4ce0cbdd2ab0
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2020-06-28 12:32:19 -0300

    roteamento no react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
+import Routes from './routes';
 
 function App() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
