66e740d2aec3d15e6dc8cf76b4a7cd91767c849b
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2020-06-28 18:20:38 -0300

    defininfo proptypes

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import api from '../../services/api';
 
 export default class Repository extends Component {
+  // eslint-disable-next-line react/static-property-placement
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  };
+
   constructor(props) {
     super(props);
     this.state = {
